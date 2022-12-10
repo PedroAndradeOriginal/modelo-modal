179ebc33e635926c1bd0e6a4e09c3bfefb53e3c9
Pedro Andrade <pedrofreitasdeandrade@gmail.com>
2022-12-10 13:49:42 -0300

    Ajuste para quantidade de cards

--- script.js
+++ script.js
@@ -1,14 +1,48 @@
-let card = document.querySelector('.card-img01');
-let modal = document.querySelector('.modal');
-let botao = document.querySelector('#botao-fechar');
+// ========== declarando variáveis
 
+let card1 = document.querySelector('#card01');
+let card2 = document.querySelector('#card02');
+let card3 = document.querySelector('#card03');
 
-card.addEventListener('click', abreModal, false);
-function abreModal() {
-    modal.classList.toggle('modal-troca')
+let modal1 = document.querySelector('#modal01');
+let modal2 = document.querySelector('#modal02');
+let modal3 = document.querySelector('#modal03');
+
+let botao1 = document.querySelector('#botao-fechar01');
+let botao2 = document.querySelector('#botao-fechar02');
+let botao3 = document.querySelector('#botao-fechar03');
+
+// ============= desclarando eventos
+
+card1.addEventListener('click', abreModal1, false);
+card2.addEventListener('click', abreModal2, false);
+card3.addEventListener('click', abreModal3, false);
+botao1.addEventListener('click', fechaModal1, false);
+botao2.addEventListener('click', fechaModal2, false);
+botao3.addEventListener('click', fechaModal3, false);
+
+// =============== funções para abrir modal
+
+function abreModal1() {
+    modal1.id = 'modal-visivel';
 }
 
-botao.addEventListener('click', fechaModal, false);
-function fechaModal() {
-    modal.classList.toggle('modal-troca')
+function abreModal2() {
+    modal2.id = 'modal-visivel';
+}
+
+function abreModal3() {
+    modal3.id = 'modal-visivel';
+}
+
+// =============== funções para fechar modal
+
+function fechaModal1() {
+    modal1.id = 'modal01';
+}
+function fechaModal2() {
+    modal2.id = 'modal02';
+}
+function fechaModal3() {
+    modal3.id = 'modal03';
 }
